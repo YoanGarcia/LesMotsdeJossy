459cf117e7acbf1bb55518810323527a974e4d61
YoanGarcia <yoan.gcia@hotmail.fr>
2016-08-30 20:30:25 +0200

    eza

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Les Mots de Jossy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="language" content="fr">
+    <meta name="author" content="Yoan Garcia, Alexis Meunier">
+    <meta name="description" content="Site de l'association LesMotsdeJossy">
+    <meta name="copyright" content="Yoan Garcia, Alexis Meunier">
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font axesome -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <!-- CSS personnalisé -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navbar -->
+    <nav id="custom-nav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid backgroundNav">
+            <div class="navbar-header page-scroll">
+                <a class="navbar-brand page-scroll" href="index.php">
+                <img id="logo" alt="Brand" src="img/logoAsso.png" width="100px"></a>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <br>
+            <div class="inlineBlock textcenter containerTheme">
+                <a class="inlineblock" href="theme1.php">
+                    <div class="divTheme">
+                        <h2>Thème 1</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme2.php">
+                    <div class="divTheme">
+                        <h2>Thème 2</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 3</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 4</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 5</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 6</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 7</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 8</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 9</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 10</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 11</h2>
+                    </div>
+                </a>
+                <a class="inlineblock" href="theme3.php">
+                    <div class="divTheme">
+                        <h2>Thème 12</h2>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Section News -->
+    <section>
+        
+    </section>
+
+    <!-- Section Info -->
+    <section>
+        
+    </section>
+
+    <!-- Section NousContacter -->
+    <section>
+        
+    </section>
+
+    <!-- Footer -->
+    <footer>
+        
+    </footer>
+
+    <!-- JS personnalisé -->
+    <script src="js/script.js"></script>
+    <script src="js/bootstrap.js"></script>
+
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,28 @@
+// Navbar //
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $('#custom-nav').addClass('affix');
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $('#custom-nav').removeClass('affix');
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// Smooth scroll //
+$(function() {
+    $('.nav a').bind('click',function(event){
+        var $anchor = $(this);
+        /*
+        if you want to use one of the easing effects:
+        $('html, body').stop().animate({
+            scrollLeft: $($anchor.attr('href')).offset().left
+        }, 1500,'easeInOutExpo');
+         */
+        $('html, body').stop().animate({
+            scrollLeft: $($anchor.attr('href')).offset().left
+        }, 1000);
+        event.preventDefault();
+    });
+});
