e16de625143fefaefa4a080db8d7199fafdcf690
YoanGarcia <yoan.gcia@hotmail.fr>
2016-08-31 10:25:22 +0200

    eza

--- index.php
+++ index.php
@@ -22,10 +22,10 @@
     <link href="css/style.css" rel="stylesheet">
 </head>
 
-<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<body id="page-top" data-spy="scroll">
 
     <!-- Navbar -->
-    <nav id="custom-nav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <nav id="custom-nav" role="navigation">
         <div class="container-fluid backgroundNav">
             <div class="navbar-header page-scroll">
                 <a class="navbar-brand page-scroll" href="index.php">
@@ -104,6 +104,34 @@
 
     <!-- Section News -->
     <section>
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                <li data-target="#myCarousel" data-slide-to="1"></li>
+                <li data-target="#myCarousel" data-slide-to="2"></li>
+                <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner" role="listbox">
+                <div class="item active">
+                    <img src="img/photo/slide1.jpg">
+                </div>
+
+                <div class="item">
+                    <img src="img/photo/slide2.png">
+                </div>
+
+                <div class="item">
+                    <img src="img/photo/slide3.png">
+                </div>
+
+                <div class="item">
+                    <img src="img/photo/slide4.jpg">
+                </div>
+            </div>
+        </div>
         
     </section>
 
@@ -112,14 +140,56 @@
         
     </section>
 
-    <!-- Section NousContacter -->
-    <section>
-        
+    <!-- Contact -->
+    <section id="contact" class="contact">
+        <div class="container-fluid">
+            <div class="row text-center">
+                <h2>Nous contacter</h2>
+
+                <form method="POST">
+                    <label for="lastname">Nom</label>
+                    <input type="text" name="lastname" id="lastname" placeholder="Entrez votre nom">
+
+                    <br>
+                    <label for="firstname">Prénom</label>
+                    <input type="text" name="firstname" id="firstname" placeholder="Entrez votre prénom">                    
+
+                    <br>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" placeholder="Entrez votre email">
+
+                    <br>
+                    <label for="phone">Téléphone</label>
+                    <input type="email" name="email" id="email" placeholder="Entrez votre email">
+
+                    <br><br>
+                    <label for="message" class="txtgrey">Message</label><br>
+                    <textarea type="text" name="message" rows="4" cols="40" placeholder="Entrez votre message"></textarea>
+
+                    <br><br>
+                    <input type="submit" value="Envoyer" class="bouton_inscription">
+            </form>
+                
+            </div>
+        </div>
     </section>
 
     <!-- Footer -->
     <footer>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-10 col-lg-offset-1 text-center">
+                    <h4><strong>Les Mots de Jossy</strong></h4>
+                    <ul class="list-unstyled">
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i>adresse</li>
+                        <li><i class="fa fa-phone-square" aria-hidden="true"></i>telephone</li>
+                        <li><i class="fa fa-envelope-square" aria-hidden="true"></i>lesmotsdejossy@orange.fr</li>
+                        </li>
+                    </ul>
+                    <p>Copyright 2016 &copy; LesMotsdeJossy</p>
+                </div>
+            </div>
+        </div>
     </footer>
 
     <!-- JS personnalisé -->
