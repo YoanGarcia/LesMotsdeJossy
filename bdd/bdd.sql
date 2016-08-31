87d85230df1c41d7640a06ecf533e2c1593ceda9
AlexisMeunier <meunier_33@live.fr>
2016-08-31 12:16:51 +0200

    aze

--- bdd/bdd.sql
+++ bdd/bdd.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 29 Août 2016 à 17:28
--- Version du serveur :  10.1.13-MariaDB
--- Version de PHP :  5.6.23
+-- Généré le :  Mer 31 Août 2016 à 12:16
+-- Version du serveur :  10.1.10-MariaDB
+-- Version de PHP :  5.6.15
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,43 +17,51 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `MDJ`
+-- Base de données :  `mdj`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `users`
+-- Structure de la table `agenda`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `agenda` (
   `id` int(11) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `token` varchar(255) NOT NULL,
-  `token_expire` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `post_date` datetime NOT NULL,
+  `exp_date` datetime NOT NULL,
+  `theme` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `agenda`
+--
+
+INSERT INTO `agenda` (`id`, `title`, `content`, `post_date`, `exp_date`, `theme`) VALUES
+(1, 'zer', 'zrezerzerzerzrezer', '2016-08-05 00:19:00', '2016-08-06 07:36:41', 'testa'),
+(2, 'zerzerzerdfsdfsdf', 'sdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdf', '2016-08-21 02:16:31', '2016-08-13 15:18:07', 'testa');
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `users`
+-- Index pour la table `agenda`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
 --
--- AUTO_INCREMENT pour la table `users`
+-- AUTO_INCREMENT pour la table `agenda`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `agenda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
