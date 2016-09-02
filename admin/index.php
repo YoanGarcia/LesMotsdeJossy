1112a07adda34a31edbf10c5862049423a7740f0
AlexisMeunier <meunier_33@live.fr>
2016-09-02 12:28:00 +0200

    aze

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Admin</title>
+    </head>
+    <body>
+        <form method="post">
+            <label for="connexion_email">Email</label>
+            <input id="connexion_email" type="email" name="connexion_email">
+            <br>
+            <label for="connexion_password">Mots de passe</label>
+            <input id="connexion_password" type="password" name="connexion_password">
+            <br>
+            <input type="submit" value="Connexion">
+        </form>
+    </body>
+</html>
