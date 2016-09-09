83c91732c6ff49dc90fc58e7a7b6d38e602b3451
AlexisMeunier <meunier_33@live.fr>
2016-09-09 19:00:55 +0200

    aze

--- admin/index.php
+++ admin/index.php
@@ -14,39 +14,28 @@
     <aside>
         <nav>
             <ul>
-                <li><a href="http://www.google.fr" class="theme_1">1</a></li>
-                <li><a href="http://www.google.fr" class="theme_2">2</a></li>
-                <li><a href="http://www.google.fr" class="theme_3">3</a></li>
-                <li><a href="http://www.google.fr" class="theme_4">4</a></li>
-                <li><a href="http://www.google.fr" class="theme_5">5</a></li>
-                <li><a href="http://www.google.fr" class="theme_6">6</a></li>
-                <li><a href="http://www.google.fr" class="theme_7">7</a></li>
-                <li><a href="http://www.google.fr" class="theme_8">8</a></li>
-                <li><a href="http://www.google.fr" class="theme_9">9</a></li>
-                <li><a href="http://www.google.fr" class="theme_10">10</a></li>
-                <li><a href="http://www.google.fr" class="theme_11">11</a></li>
-                <li><a href="http://www.google.fr" class="theme_12">12</a></li>
+                <li><a href="#" class="theme" data-theme="retraite">Animation Retraite</a></li>
+                <li><a href="#" class="theme" data-theme="spe">Animation S.P.E</a></li>
+                <li><a href="#" class="theme" data-theme="as">A.S</a></li>
+                <li><a href="#" class="theme" data-theme="cocci">Cocciabeille</a></li>
+                <li><a href="#" class="theme" data-theme="etpuis">EtPuis</a></li>
+                <li><a href="#" class="theme" data-theme="fikidi">Fikidi</a></li>
+                <li><a href="#" class="theme" data-theme="francais">Francais</a></li>
+                <li><a href="#" class="theme" data-theme="manif">Manif</a></li>
+                <li><a href="#" class="theme" data-theme="pp">P.P</a></li>
+                <li><a href="#" class="theme" data-theme="reaap">R.E.A.A.P</a></li>
+                <li><a href="#" class="theme" data-theme="tap">T.A.P</a></li>
+                <li><a href="#" class="theme" data-theme="theatre">Theatre</a></li>
             </ul>
         </nav>
     </aside>
     <script>
-        var theme_1 = document.querySelector('.theme_1');
-        var theme_2 = document.querySelector('.theme_2');
-        var theme_3 = document.querySelector('.theme_3');
+        var lien = document.querySelectorAll('.theme');
 
-        theme_1.addEventListener('click', function(e){
-            e.preventDefault();
-            console.log('click theme_1');
-        });
-
-        theme_2.addEventListener('click', function(e){
-            e.preventDefault();
-            console.log('click theme_2');
-        });
-
-        theme_3.addEventListener('click', function(e){
-            e.preventDefault();
-            console.log('click theme_3');
+        lien.forEach(function(value, index, array){
+            value.addEventListener('click', function(){
+                console.log(this.getAttribute('data-theme'));
+            });
         });
     </script>
     <main>
