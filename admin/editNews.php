c4f6d4d39e26859a79ff3229d422e2bb523c2064
AlexisMeunier <meunier_33@live.fr>
2016-09-22 00:52:34 +0200

    aze

--- admin/editNews.php
+++ admin/editNews.php
@@ -5,13 +5,54 @@
 
     $controller->auth();
 
+
     if(!empty($_POST)){
         $post = array_map('trim', array_map('strip_tags', $_POST));
 
+        if(isset($post['form'])){
+            if($post['form'] == 'addActivite'){
+                $type = 'image';
+                $imageFinale = '';
+                $folder = '../img/activites/'.$post['theme'].'/';
 
+                if(!empty($_FILES)){
+                    if(isset($_FILES['picture']) && $_FILES['picture']['error'] == UPLOAD_ERR_OK) {
+                        $fileName = $_FILES['picture']['name']; // Nom de mon image
+                        $fileTemp = $_FILES['picture']['tmp_name']; // Image temporaire
+
+                        $newFileName = explode('.', $fileName);
+                        $fileExtension = end($newFileName); // Récupère l'extension du fichier
+
+                        $finalFileName = $post['theme'].'-'.time().'.'.$fileExtension;
+
+                        if(move_uploaded_file($fileTemp, $folder.$finalFileName)) {
+                            // Ici je suis sur que mon image est au bon endroit
+                            $imageFinale = 'img/activites/'.$post['theme'].'/'.$finalFileName;
+                        }
+                    }
+                }
+
+                if(isset($post['video'])){
+                    if($post['video'] == '1'){
+                        $type = 'video';
+                    }
+                    else{
+                        $type = 'image';
+                    }
+                }
+
+                if($type = 'image'){
+                    $link = $imageFinale;
+                }
+                elseif($type = 'video'){
+                    $link = $post['video'];
+                }
+
+                $controller->addActivites($post['title'], $link, $post['theme'], $type);
+            }
+        }
     }
 ?>
-
 
 <!DOCTYPE html>
 
@@ -73,7 +114,7 @@
 
     <div class="col-lg-12 textcenter">    
         <a href="#" class="divEditTheme" data-theme="acceuil">Accueil</a>
-        <a href="#" class="divEditTheme" data-theme="test">Animation Retraite</a>
+        <a href="#" class="divEditTheme" data-theme="retraite">Animation Retraite</a>
         <a href="#" class="divEditTheme" data-theme="spe">Animation S.P.E</a>
         <a href="#" class="divEditTheme" data-theme="as">A.S</a>
         <a href="#" class="divEditTheme" data-theme="cocci">Cocciabeille</a>
@@ -97,6 +138,7 @@
         <h2>activités</h2>
         <br><br>
         <form method="post">
+            <input type="hidden" name="form" value="addActivite">
             <input type="hidden" name="theme" class="input_theme" value="">
             <input type="text" name="title" placeholder="Nom de l'activité">
             <input type="text" name="link" id="link_vid" style="display: none;">
